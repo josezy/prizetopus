a30b7c7e436767c74510291ff4c3e3c17554e777
Michał Flak <michal.flak.96@gmail.com>
2023-03-17 16:00:48 -0600

    tournament struct

--- programs/prizetopus/src/state/tournament.rs
+++ programs/prizetopus/src/state/tournament.rs
@@ -1,1 +1,13 @@
 use anchor_lang::prelude::*;
+
+#[account]
+#[derive(Debug)]
+pub struct Tournament {
+    pub authority: Pubkey,
+    pub prize_pool: Pubkey,
+    pub prize_distribution: Pubkey,
+    pub buy_in: u64,
+    pub max_players: u64,
+    pub started: bool,
+    pub timeout: u64,
+}
