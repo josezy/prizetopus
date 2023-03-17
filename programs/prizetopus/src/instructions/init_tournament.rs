b6add8513de90e285655b6e8d08f35ce7a06840a
Michał Flak <michal.flak.96@gmail.com>
2023-03-17 16:26:43 -0600

    ffsdfsdf

--- programs/prizetopus/src/instructions/init_tournament.rs
+++ programs/prizetopus/src/instructions/init_tournament.rs
@@ -10,17 +10,49 @@ pub struct InitTournament<'info> {
     #[account(init, payer = payer, space = 8 + std::mem::size_of::<Tournament>())]
     pub tournament: Account<'info, Tournament>,
 
-    pub tournament_authority: Account<'info>,
+    pub tournament_authority: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
 
     // Prize pool
-    #[account(mut, seeds=[PREFIX.as_bytes(), tournament.key().as_ref(), b"prize_pool".as_ref()], bump)]
-    pub prize_pool: Account<'info>,
+    #[account(
+        init, 
+        seeds=[
+            PREFIX.as_bytes(), 
+            tournament.key().as_ref(), 
+            b"prize_pool".as_ref()
+            ], 
+        bump,
+        payer = payer,
+        space = 8
+    )]
+    pub prize_pool: UncheckedAccount<'info>,
+
     #[account(
         init,
-        seeds = [ PREFIX.as_bytes(), tournament.key(),b"prize_distribution".as_ref()],
-        bump = bump,
+        seeds = [ 
+            PREFIX.as_bytes(), 
+            tournament.key().as_ref(),
+            b"prize_distribution".as_ref()
+            ],
+        bump,
         payer = payer,
         space = 8 + std::mem::size_of::<PrizeDistribution>()
     )]
     pub prize_distribution: Account<'info, PrizeDistribution>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+pub fn handler(
+    ctx: Context<InitTournament>,
+    prize_pool_bump: u8,
+    prize_distribution_bump: u8,
+    buy_in: u64,
+    max_players: u64,
+    timeout: u64,
+) -> Result<()> {
+
+    Ok(())
 }
--- programs/prizetopus/src/state/mod.rs
+++ programs/prizetopus/src/state/mod.rs
@@ -1,2 +1,6 @@
 pub mod tournament;
+pub mod player_state;
+pub mod prize_distribution;
 pub use tournament::*;
+pub use player_state::*;
+pub use prize_distribution::*;
